dea8bd3e82799d0bd046aeb984057db0559b8b2f
Antony <antonygmurimi@gmail.com>
2024-03-27 12:51:20 +0300

    js code to multiply, get random no,mod and max

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+// Write your code her
+
+//multiply variables
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+ //random above 0
+let random;
+random = Math.floor(Math.random() * 10) + 1;
+
+
+//calculate remaider
+let num3 = 19;
+let num4 = 5;
+let mod = num3 % num4;
+
+//max number
+let max;
+max = Math.max(15,16,17,18,19,20);
